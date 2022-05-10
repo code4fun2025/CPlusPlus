e7c882f53d0fbb409f19db13e2e7123127447ee8
code4fun2025 <50508650+code4fun2025@users.noreply.github.com>
2022-05-10 00:52:24 -0700

    Create 207.course.schedule.cpp

---
+++ 207.course.schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> preMap(numCourses, vector<int>());
+        //map each course to prereq list
+        for(vector<int>& pre : prerequisites)
+            preMap[pre[0]].push_back(pre[1]);
+
+        // visited state: 0 = unknown, 1 = visiting, 2 = visited/clear
+        // initialize the visited table with 0
+        vector<int> visited(numCourses, 0);
+        
+        // check every course, in case for isolated course couse 1->2
+        for(int i = 0; i < numCourses; ++i)
+            if(!dfs(preMap, visited, i)) return false;
+        
+        return true;
+
+    }
+    
+    bool dfs(vector<vector<int>>& preMap, vector<int>& visited, int crs)
+    {
+        // found loop/circle, return true 
+        if (visited[crs] == 1) 
+            return false;
+        // found visited/clear state, return true 
+        if (visited[crs] == 2)
+            return true;
+        
+        // mark we are visiting the course
+        visited[crs] = 1;
+        for(int pre: preMap[crs])
+            // return if there's any prereq returns false
+            if(!dfs(preMap, visited, pre)) 
+                return false;
+        
+        // no loop, mark the course to be visisted/cleared
+        visited[crs] = 2;
+        
+        return true;
+    }
+};
