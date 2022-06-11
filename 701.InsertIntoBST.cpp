5f18017e5c2cae64ebc5a3f81ac0203b8677856a
code4fun2025 <50508650+code4fun2025@users.noreply.github.com>
2022-06-10 23:54:32 -0700

    Create 701.InsertIntoBST.cpp

---
+++ 701.InsertIntoBST.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == nullptr)
+            return new TreeNode(val);
+        
+        if (val > root->val)
+            root->right = insertIntoBST(root->right, val);
+        else
+            root->left = insertIntoBST(root->left, val);
+        
+        return root;
+    }
+};
